d0e0f25737f46d1e933b21553e929397d5f1eb18
HatemBarghathi <hbarghathih@gmail.com>
2021-03-25 08:36:27 -0500

    Add files via upload

--- CodeForFreeFermionsOEE/FFOEE_main.jl
+++ CodeForFreeFermionsOEE/FFOEE_main.jl
@@ -1,12 +1,11 @@
 # Renyi entanglement entropy of Free fermions in 1D.
-#julia FFOEE_main.jl  --pbc  --l-step 1 --l-num 100  --l-min 1  200 100  --alpha 2 --out L200N100alpha2.dat
-#julia FFOEE_main.jl  --pbc  --precision 1000 --l-log --l-logstep 0.1 --l-min 5 --l-max 1000  1000 500  --alpha 2 --out L1000N500alpha2.dat
 
 push!(LOAD_PATH, joinpath(dirname(@__FILE__), "src"))
 using FreeFermionsOEE
 
 using ArgParse
-#using JeszenszkiBasis
+using Printf
+using LinearAlgebra
 
 s = ArgParseSettings()
 s.autofix_names = true
@@ -25,23 +24,6 @@ s.autofix_names = true
         required = true
 end
 
-add_arg_group(s, "boundary conditions")
-@add_arg_table s begin
-    "--pbc"
-        help = "periodic boundary conditions (default)"
-        arg_type = BdryCond
-        action = :store_const
-        dest_name = "boundary"
-        constant = PBC
-        default = PBC
-    "--obc"
-        help = "open boundary conditions"
-        arg_type = BdryCond
-        action = :store_const
-        dest_name = "boundary"
-        constant = OBC
-        default = PBC
-end
 add_arg_group(s, "BH parameters")
 @add_arg_table s begin
     "--l-min"
@@ -51,6 +33,7 @@ add_arg_group(s, "BH parameters")
         default = 1
     "--l-max"
         metavar = "l"
+        help = "maximum lA"
         arg_type = Int
         default = 1
     "--l-step"
@@ -81,74 +64,76 @@ add_arg_group(s, "BH parameters")
         help = "alpha value"
         arg_type = Float64
         default = 2.0
-
+    "--eigs"
+        help = "report the spectrum of the projected correlation matrix."
+        action = :store_true
+    "--prob"
+        help = "report the particle-number probability distribution."
+        action = :store_true
 
 end
 
-c = parsed_args = parse_args(ARGS, s, as_symbols=true)
-# Number of sites
-const L = c[:L]
-# Number of particles
-const N = c[:N]
-# Output file
-const output = c[:out]
-# Boundary conditions
-const boundary = c[:boundary]
-# minimum lA
-const lA_min = c[:l_min]
-#precision
-const precision=c[:precision]
-const alpha=c[:alpha]
+function main()
+
+   c = parsed_args = parse_args(ARGS, s, as_symbols=true)
+   # Number of sites
+   L = c[:L]
+   # Number of particles
+   N = c[:N]
+   # Output file
+   output = c[:out]
+   # minimum lA
+   lA_min = c[:l_min]
+   #precision
+   precision=c[:precision]
+   alpha=c[:alpha]
+   eigenvalues= c[:eigs]
+   probabilities= c[:prob]
 
 
 
-if c[:l_log]
-   l_cut=trunc(Int,1.0/(exp(c[:l_logstep])-1.0))+1 
-   if lA_min<l_cut
-      loglnum=trunc(Int,log(c[:l_max]*1.0/l_cut)/c[:l_logstep])
-      l_range = zeros(Int, l_cut-lA_min+1+ loglnum)
-      for i=1: l_cut-lA_min+1
-         l_range[i]=i+ lA_min-1
-      end
-      for i=1:loglnum
-         l_range[i+l_cut-lA_min+1]= trunc(Int,l_cut*exp(i*c[:l_logstep]))
-      end
-      lA_max=l_range[l_cut+ loglnum-lA_min]
+   if c[:l_log]
+      l_cut=trunc(Int,1.0/(exp(c[:l_logstep])-1.0))+1 
+      if lA_min<l_cut
+         loglnum=trunc(Int,log(c[:l_max]*1.0/l_cut)/c[:l_logstep])
+         l_range = zeros(Int, l_cut-lA_min+1+ loglnum)
+         for i=1: l_cut-lA_min+1
+            l_range[i]=i+ lA_min-1
+         end
+         for i=1:loglnum
+            l_range[i+l_cut-lA_min+1]= trunc(Int,l_cut*exp(i*c[:l_logstep]))
+         end
+         lA_max=l_range[l_cut+ loglnum-lA_min]
+      else
+         loglnum=trunc(Int,log(c[:l_max]*1.0/lA_min)/c[:l_logstep])
+         l_range = zeros(Int, loglnum+1)
+         for i=1: loglnum+1
+            l_range[i]= trunc(Int,lA_min*exp((i-1)*c[:l_logstep]))
+         end
+         lA_max=l_range[loglnum+1]
+      end 
    else
-      loglnum=trunc(Int,log(c[:l_max]*1.0/lA_min)/c[:l_logstep])
-      l_range = zeros(Int, loglnum+1)
-      for i=1: loglnum+1
-         l_range[i]= trunc(Int,lA_min*exp((i-1)*c[:l_logstep]))
-      end
-      lA_max=l_range[loglnum+1]
-   end 
-else
-   lA_max=(lA_min+c[:l_step]*(c[:l_num]-1))
-   l_range = lA_min:c[:l_step]:(lA_min+c[:l_step]*(c[:l_num]-1))
-end
+      lA_max=(lA_min+c[:l_step]*(c[:l_num]-1))
+      l_range = lA_min:c[:l_step]: lA_max
+   end
 #_______________________
-open(output, "w") do f
-   write(f, "# L=$(L), N=$(N), $(boundary), precision=$(precision), alpha=$(alpha) \n")
-   write(f, "# l    S1    S1_OP    S_alpha    S_alpha_OP(1)    sigma2_n H_alpha navge_alpha S_alpha_OP(3) S_alpha_OP(4) S_alpha_OP(5) \n")
+   open(output, "w") do f
+      write(f, "# L=$(L), N=$(N), precision=$(precision), alpha=$(alpha) \n")
+      write(f, @sprintf "#%7s%24s%24s%24s%24s%24s%24s%24s%24s%24s%24s\n" "l"    "S1"    "S1_OP"    "S_alpha"    "S_alpha_OP(1)"    "sigma2_n" "H_alpha" "navge_alpha" "S_alpha_OP(3)" "S_alpha_OP(4)" "S_alpha_OP(5)")
 
    for l in l_range
-	# ======================= Correlation matrix =============================
-	if boundary==PBC
-	Corr_Matrix = zeros(Float64, l, l)
-	phase_factor= Float64
-	even_odd=N % 2 
-	phase_factor=2*pi/L 
-	for i=1: l
- 	  for j=i+1: l
-	         Corr_Matrix[i,j]=sin(N*phase_factor/2*(i-j))/sin(phase_factor/2*(i-j))/L
-	         Corr_Matrix[j,i]=Corr_Matrix[i,j]
-	   end
- 	  Corr_Matrix[i,i]=N/L
-	end
-	elseif boundary==OBC
-	   warn(" op is not supported, yet. ")
-	   exit(1)
-	end
+      # ======================= Correlation matrix =============================
+      Corr_Matrix = zeros(Float64, l, l)
+      phase_factor= Float64
+      even_odd=N % 2 
+      phase_factor=2*pi/L 
+      for i=1: l
+         for j=i+1: l
+            Corr_Matrix[i,j]=sin(N*phase_factor/2*(i-j))/sin(phase_factor/2*(i-j))/L
+            Corr_Matrix[j,i]=Corr_Matrix[i,j]
+         end
+         Corr_Matrix[i,i]=N/L
+      end
 	# ==================================End===================================
 
         # Create C_A
@@ -165,10 +150,12 @@ open(output, "w") do f
         end
 
  # Calculate spatial & operational entanglement entropy
-        s1_spatial, s1_operational, salpha_spatial, salpha_operational, sigma2_n, LogSumPNalpha, H4, H5, H6 = operational_entanglement(L, N , l, precision, alpha, nu_increasing, boundary=boundary)
-        write(f, @sprintf "%d %.15E %.15E %.15E %.15E %.15E %.15E %.15E %.15E %.15E %.15E \n" l  s1_spatial s1_operational salpha_spatial salpha_operational sigma2_n LogSumPNalpha navge_alpha  H4 H5 H6)
+        s1_spatial, s1_operational, salpha_spatial, salpha_operational, sigma2_n, LogSumPNalpha, H4, H5, H6 = operational_entanglement(L, N , l, precision, alpha, nu_increasing, eigenvalues, probabilities)
+        write(f, @sprintf "%8s%24.12E%24.12E%24.12E%24.12E%24.12E%24.12E%24.12E%24.12E%24.12E%24.12E\n" "$(l)"  s1_spatial s1_operational salpha_spatial salpha_operational sigma2_n LogSumPNalpha navge_alpha  H4 H5 H6)
 
         flush(f)
    end
-
+   end
 end
+
+main()
